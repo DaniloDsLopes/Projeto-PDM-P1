a56108215016299e9edc472acbb9e20e6451a8ae
Danilo Lopes <danilo.dslopes@gmail.com>
2024-04-19 17:41:51 -0300

    feat(p1-pdmn):exibe lista ficticia

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom';
 import React from 'react'
 import LembreteEntrada from './LembreteEntrada';
+import LembreteLista from './LembreteLista';
 const App = () => {
     return (
-        <div className='container-fluid mt-3 '>
-                <div className='justify-content-md-center'>
-                    <LembreteEntrada/>
-                </div>
-        </div>
+        <div className="container-fluid mt-3">
+            <div className="justify-content-md-center">
+                <LembreteLista
+                    Lembrete="Preparar aula de progrmação"
+                />
+                <LembreteLista
+                    Lembrete="Fazer feira"
+                />
+                <LembreteLista
+                    Lembrete="Preparar marmitas"
+                />
+            </div>
+            <script>
 
+            </script>
+            <div className='justify-content-md-center'>
+                <LembreteEntrada />
+            </div>
+
+
+        </div>
     )
 }
 
