d32d498cd126b324dff79e127b61441b9f3d5def
Danilo Lopes <danilo.dslopes@gmail.com>
2024-04-19 17:27:11 -0300

    feat(p1-pdmt):cria componente principal efaz hello

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom';
+import React from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+    return (
+        <div className='container-fluid mt-3 '>
+            <div className="row">
+                <div className="col-sm-12 col-md-9 col-lg-7 col-xl-6 text-center">
+                    <h1>Hello, lembretes</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
