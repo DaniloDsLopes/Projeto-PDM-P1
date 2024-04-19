69b92e9f2c8e40e028bdbc26fff29b8bd4b6d4fd
Danilo Lopes <danilo.dslopes@gmail.com>
2024-04-19 17:37:21 -0300

    feat(p1-pdmn):cria componente de entrada

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom';
 import React from 'react'
-
+import LembreteEntrada from './LembreteEntrada';
 const App = () => {
-
-
     return (
         <div className='container-fluid mt-3 '>
-            <div className="row">
-                <div className="col-sm-12 col-md-9 col-lg-7 col-xl-6 text-center">
-                    <h1>Hello, lembretes</h1>
+                <div className='justify-content-md-center'>
+                    <LembreteEntrada/>
                 </div>
-            </div>
         </div>
+
     )
 }
 
